873362ed380925cf4000257ac73fac6b17aaf0ef
Naufal <ahmadnaufalalghifari@gmail.com>
2025-03-16 11:47:30 +0700

    Generic

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class main
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric a = new HaloGeneric();
+        a.sapaUser<string>("Naufal");
+        DataGeneric<string> b = new DataGeneric<string>("103022300006");
+        b.printData();
+    }
+}
+
+class DataGeneric<T>
+{
+    T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class HaloGeneric()
+{
+    public void sapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo User {user}");
+    }
+}
